251bd12eb2d88ded20200963e9d6ac3ceb76d044
Alvaro <elsociosm@gmail.com>
2016-11-17 17:02:53 +0100

    Implementado script que recoge datos

---
+++ datos.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ *Script que recoge los datos del formulario y los muestra por pantalla
+ */
+
+$name = $_POST["nombre"];
+$age = $_POST["edad"];
+$city = $_POST["ciudad"];
+$sex = $_POST["sexo"];
+
+echo "<p>Datos del usuario registrado en el formulario:</p>";
+echo "<p>Nombre: $name Edad: $age Ciudad: $city Sexo: $sex</p>";
