5fa0ce5036da3fa49a59541698d6157b13280dca
Alvaro <elsociosm@gmail.com>
2016-11-17 16:47:29 +0100

    Formulario html creado

--- index.php
+++ index.php
@@ -7,11 +7,24 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Ejemplo de formulario de alta</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="datos.php" method="POST">
+            <p>Nombre: <input type="text" name="nombre"></p>
+            <p>Edad: <input type="number" name="edad"></p>
+            <p>Ciudad: <select name="ciudad"></p>
+                <option>Barcelona</option>
+                <option>Girona</option>
+                <option>Lleida</option>
+                <option>Tarragona</option>
+                </select>
+            <p>Sexo:
+                Hombre:<input type="radio" name="sexo" value="hombre">
+                Mujer:<input type="radio" name="sexo" value="mujer"></p>
+                <input type="submit" name="enviar" value="Enviar datos">
+        </form>
+        
+        
     </body>
 </html>
